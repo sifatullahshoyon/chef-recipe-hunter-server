93730a7be3a18d4d1d4139608407337e36a45a0d
freelancersifat627e <freelancersifat627@gmail.comm>
2024-01-08 16:47:26 +0600

    some code updated

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const port = process.env.PORT || 5000 ;
 
 app.use(cors());
 
+const chefs = require('./data/chef.json');
+
 app.get('/' , (req , res) => {
     res.send('chef recipe hunter in running on server')
 });
+
+
+app.get('/chefs' , (req , res) => {
+    res.send(chefs);
+});
+
+
 
 app.listen(port , () => {
     console.log(`Server is running on Port : ${port} `);
