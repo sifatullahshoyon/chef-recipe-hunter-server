bfe872e4c1e8910b27c0c6d2c349500002627ac9
freelancersifat627e <freelancersifat627@gmail.comm>
2024-01-13 01:21:42 +0600

    code updated

--- index.js
+++ index.js
@@ -1,42 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const chefs = require('./data/chef.json');
+const chefs = require("./data/chef.json");
 
-const singleChefs = require('./data/singleChefData.json');
+const singleChefs = require("./data/singleChefData.json");
 
-// const corsOptions = {
-//     origin: 'http://localhost:5173/',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   }
-
-app.get('/' , (req , res) => {
-    res.send('chef recipe hunter in running on server')
+app.get("/", (req, res) => {
+  res.send("chef recipe hunter in running on server");
 });
 
-
-// app.get('/chefs' , cors(corsOptions) , (req , res) => {
-//     res.send(chefs);
-// });
-
-
-app.get('/chefs' ,  (req , res) => {
-    res.send(chefs);
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
-
-app.get('/chefs/:id' , (req, res) => {
-    const id = parseInt(req.params.id);
-    const singleChefsData = singleChefs?.find(singleData => singleData.id === id);
-    res.send(singleChefsData);
+app.get("/chefs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const singleChefsData = singleChefs?.find(
+    (singleData) => singleData.id === id
+  );
+  res.send(singleChefsData);
 });
 
-
-
-app.listen(port , () => {
-    console.log(`Server is running on Port : ${port} `);
+app.listen(port, () => {
+  console.log(`Server is running on Port : ${port} `);
 });
